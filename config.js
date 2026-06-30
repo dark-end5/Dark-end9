c3a56a05dfe83320daa1751fa86f327d741f490d
dark-end5 <christtheanchormentors@gmail.com>
2026-07-01 02:09:08 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  prefix: ".",
+  botName: "✨ Lesta Bot v2",
+  ownerNumber: "254706519089",
+  owners: ["254706519089"],
+  autobioText: "HELLO KENYA 🇰🇪",
+  autobioInterval: 20000,
+  followUpMsg: "👋 Hey! I'm *Lesta Bot v2*.\nSend *.menu* to see all available commands!",
+  badWords: ["fuck", "shit", "bitch", "asshole", "bastard", "cunt", "dick", "pussy", "nigga", "whore"],
+  emojis: ["❤️", "😂", "🔥", "👍", "😍", "🎉", "💯", "✨", "🙏", "😎", "🤣", "😊", "👏", "🥰", "💪"],
+};
